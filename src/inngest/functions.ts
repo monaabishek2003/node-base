dc3b31a3a499bbc6a4c64f73dd7657e2b7d556c5
monaabishek2003 <monaabisheka@gmail.com>
2026-02-28 07:00:27 +0000

    setup: sentry setup for logs,errors,ai-sdk

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -46,7 +46,13 @@ export const execute = inngest.createFunction(
         model: google("gemini-2.5-flash"),
         system: "You are a helpful assistant.",
         prompt: "What is 2 + 2?",
+        experimental_telemetry: {
+         isEnabled: true,
+         recordInputs: true,
+         recordOutputs: true,
+  }     ,
       }
+      
     );
 
     // const { steps: openaiSteps } = await step.ai.wrap(
